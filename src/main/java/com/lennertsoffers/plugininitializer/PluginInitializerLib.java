3a1847ce0ea820d0ed4b355bae61ec2557b6e59f
lennertsoffers <lennert.soffers@hotmail.com>
2022-06-01 12:51:51 +0200

    feat: renamed project to PluginInitializerLib

--- src/main/java/com/lennertsoffers/plugininitializer/PluginInitializerLib.java
+++ src/main/java/com/lennertsoffers/plugininitializer/PluginInitializerLib.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-public class PluginInitializer {
+public class PluginInitializerLib {
     /**
      * Registers all classes annotated with:
      * <ul>
